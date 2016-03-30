995d8c6d7600c9dd1f749094c69e20f8adb72e72
Shawn Cramp <Shawn Cramp>
2016-03-30 08:52:42 -0400

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var app = require('../app');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    console.log(req.cookies['connect.sid']);
+    res.render('index', {title: 'PictoFriends', name: 'Shawn'});
+});
+
+module.exports = router;
